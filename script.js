e745dd8cdc38d4fc69a19b46c15322a6d556bf9d
gansotanaka <gansotanaka65@gmail.com>
2022-12-13 21:03:40 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 let lists = document.getElementsByClassName("pull-down-list");
 console.log(lists)
+window.addEventListener('load', function(){
+    var li = document.getElementById("lists")
+
+    const pullDownButton = document.getElementById("lists")
+
+    pullDownButton.addEventListener('mouseover', function(){
+        console.log("乗る")
+    })
+
+    li.addEventListener('click', function(){
+        console.log('クリック');
+    }, false);
+    li.click();
+
+    li.addEventListener('mouseout', function(){
+        console.log('外れる');
+    });
+     li.onmouseout();
+
+})
