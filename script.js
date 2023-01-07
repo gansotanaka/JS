00a41d3bf35e2c0021b98ff1ce08522fe22fdefa
gansotanaka <gansotanaka65@gmail.com>
2023-01-07 22:04:56 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 let lists = document.getElementsByClassName("pull-down-list");
 console.log(lists)
 window.addEventListener('load', function(){
-    var pullDownButton = document.getElementById("lists")
+    const pullDownButton = document.getElementById("lists")
+    const pullDownParents = document.getElementById("pull-down")
 
     pullDownButton.addEventListener('mouseover', function(){
-        pullDownButton.setAttribute('style', "background-color: blue;")
+        this.setAttribute('style', "background-color: blue;")
         console.log("乗った時は青");
     }, false);
 
-    pullDownButton.addEventListener('click', function(){
-        pullDownButton.style.backgroundColor = 'green';
-        console.log('クリックした時は緑');
-    }, false);
-
     pullDownButton.addEventListener('mouseout', function(){
-        pullDownButton.removeAttribute('style', "background-color: red;")
+        this.removeAttribute('style', "background-color: red;")
         console.log('外れた時は赤');
     }, false);
 
+    pullDownButton.addEventListener('click', function(){
+        if (pullDownParents.getAttribute("style")=="display:block;"){
+            pullDownParents.removeAttribute("style","display:block")
+            console.log("非表示")
+        } else {
+            pullDownParents.setAttribute("style","display:block;")
+            console.log("表示")
+        }
+    })
+    
 })
